1349bf8552ff9d1527bae858ac03b14d15b4e07a
reidjamile <reidjamile@gmail.com>
2020-04-18 22:21:17 -0400

    add turbolinks and dependent fields gems

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,8 @@
+
+//= require jquery
+//= require jquery.turbolinks
+//= require jquery_ujs
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= require dependent-fields
+//= require turbolinks
