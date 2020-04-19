5112f6110592e46048b39d622f0216d2be1eb409
reidjamile <reidjamile@gmail.com>
2020-04-19 12:05:39 -0400

    dependent form fields added

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,7 +2,9 @@
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
+//= require underscore
+//= require dependent-fields
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= require dependent-fields
 //= require turbolinks
+
