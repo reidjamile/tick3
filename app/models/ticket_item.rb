5e247aefc4b10a697ed2b98bdfa49dc5cc6ad59d
reidjamile <reidjamile@gmail.com>
2020-04-20 18:58:53 -0400

    edit page cleanup

--- app/models/ticket_item.rb
+++ app/models/ticket_item.rb
@@ -1,3 +1,12 @@
 class TicketItem < ApplicationRecord
   belongs_to :ticket_list
+
+  validates_presence_of :ttype, :message => 'Please Complete Ticket Item.'
+
+  validates :bxnum, numericality: { greater_than: -0.01 }, allow_nil: true, presence: true
+  validates :stnum, numericality: { greater_than: -0.01 }, allow_nil: true, presence: true
+  validates :starnum, numericality: { greater_than: -0.01 }, allow_nil: true, presence: true
+  validates :lotnum, numericality: { greater_than: -0.01 }, allow_nil: true, presence: true
+ 
+
 end
